a7d4c0a6573a12d33ef30000920283394f2855e3
tgold83 <ted.jonathan.gold@gmail.com>
2022-09-21 13:39:31 -0700

    add functionality for users ability to add and view lists
    co-authored-by: Helen Lehrer <hlehrer@outlook.com>
    Co-authored-by: Kelly Bruce <kelly.bruce.cs@gmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -9,36 +9,37 @@ namespace UserList
 		public static void Main()
 		{
 			Console.WriteLine("Welcome to the To Do List.");
-			Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
-			string userInput = Console.ReadLine();
-			if (userInput == "add" || userInput == "Add")
+			ListMaker();
+			
+			static void ListMaker() 
 			{
-				Console.WriteLine("Please enter the description for the new item.");
-				string itemDescription = Console.ReadLine();
-				Item newItem = new Item(itemDescription);
-				Console.WriteLine(itemDescription + " has been added to your list. Would you like to add an item to your list or view your list? (Add/View)");
-				Main();
-			}
-			else if (userInput == "view" || userInput == "View")
-			{
-				List<string> stringifiedList = new List<string> {};
-				foreach (Item item in Item.GetAll()) {
-					stringifiedList.Add(item.Description);
+				Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
+				string userInput = Console.ReadLine();
+				if (userInput == "add" || userInput == "Add")
+				{
+					Console.WriteLine("Please enter the description for the new item.");
+					string itemDescription = Console.ReadLine();
+					Item newItem = new Item(itemDescription);
+					Console.WriteLine(itemDescription + " has been added to your list. Would you like to add an item to your list or view your list? (Add/View)");
+					ListMaker();
 				}
-				string listString = string.Join(", ", stringifiedList.ToArray());
-				Console.WriteLine(listString);
-				Main();
+				else if (userInput == "view" || userInput == "View")
+				{
+					List<Item> list = Item.GetAll();
+					if (list.Count == 0)
+					{
+						Console.WriteLine("You haven't added any items to the list.");
+						ListMaker();
+					}
+					List<string> stringifiedList = new List<string> {};
+					foreach (Item item in Item.GetAll()) {
+						stringifiedList.Add(item.Description);
+					}
+					string listString = string.Join("\n", stringifiedList.ToArray());
+					Console.WriteLine(listString);
+					ListMaker();
+				}
 			}
 		}
 	}
 }
-
-// Program: Welcome to the To Do List.
-// Program: Would you like to add an item to your list or view your list? (Add/View)
-// User: Add
-// Program: Please enter the description for the new item.
-// User: Walk the dog.
-// Program: 'Walk the dog' has been added to your list. Would you like to add an item to your list or view your list? (Add/View)
-// User: View
-// Program: 1. Walk the dog.
-// Program: Would you like to add an item to your list or view your list? (Add/View)
