383022d893d6916e93e556ab65cfcbd6b9c8d2c4
tgold83 <ted.jonathan.gold@gmail.com>
2022-10-02 20:46:41 -0700

    add code from weekend hw on sql

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -39,5 +39,10 @@ namespace ToDoList
         await context.Response.WriteAsync("Hello World!");
       });
     }
+
+    public static class DBConfiguration
+    {
+      public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list;";
+    }
   }
 }
